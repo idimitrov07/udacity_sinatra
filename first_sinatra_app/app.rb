55a3a2f86928cd3c941b7dbacb3fd314c4da3cfe
Ivan Dimitrov <idimitrov07@gmail.com>
2016-08-31 21:22:55 +0300

    Add dynamic routes.

--- first_sinatra_app/app.rb
+++ first_sinatra_app/app.rb
@@ -12,6 +12,25 @@ get '/home/:name' do
   "Hello #{params['name']}"
 end
 
-get '/:name' do |n|
-  "Hello Mr. #{n}"
+get '/about' do
+  'This is our first Sinatra App.'
+end
+
+get '/roll-die' do
+  "Your die roll is...#{rand(1...7)}"
+end
+
+get '/greet/:first/:last' do
+  first = params[:first].capitalize
+  last = params[:last].capitalize
+  "Hello there #{first} #{last}! "
+end
+
+get '/square/:num' do
+  num = params[:num]
+  if  num.to_i.to_s == num
+    (num.to_i**2).to_s
+  else
+    "That's not a number"
+  end
 end
